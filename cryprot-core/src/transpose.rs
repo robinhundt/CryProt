c33e48957323ef4af9a0b00b193351f9290db4f4
robinhundt <24554122+robinhundt@users.noreply.github.com>
2025-02-21 17:56:58 +0100

    cryprot-core: fix avx2 transpose portability

--- cryprot-core/src/transpose.rs
+++ cryprot-core/src/transpose.rs
@@ -1,10 +1,22 @@
-// #[cfg(target_feature = "avx2")]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub mod avx2;
 pub mod portable;
 
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+cpufeatures::new!(target_feature_avx2, "avx2");
+
+/// Transpose bit matrix.
+///
+/// # Panics
+/// If `rows % 128 != 0`
+/// If for `let cols = input.len() * 8 / rows`, `cols % 128 != 0`
 pub fn transpose_bitmatrix(input: &[u8], output: &mut [u8], rows: usize) {
-    #[cfg(target_feature = "avx2")]
-    avx2::transpose_bitmatrix(input, output, rows);
-    #[cfg(not(target_feature = "avx2"))]
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    if target_feature_avx2::get() {
+        unsafe { avx2::transpose_bitmatrix(input, output, rows) }
+    } else {
+        portable::transpose_bitmatrix(input, output, rows);
+    }
+    #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
     portable::transpose_bitmatrix(input, output, rows);
 }
